c98275c46ef47df076dd76da5f7d7209ee0adce7
Craig Partin <craigpartin@Craig-Partins-MacBook-Pro.local>
2009-10-16 16:59:14 -0600

    finish template and file copying

--- lib/template.rb
+++ lib/template.rb
@@ -1,46 +1,16 @@
+gem 'contentator'
+gem 'kete-tiny_mce', :lib => 'tiny_mce'
+gem 'thoughtbot-shoulda', :lib => 'shoulda'
+gem 'thoughtbot-factory_girl', :lib => 'factory_girl'
+gem 'haml',
+gem 'mocha',
+
+run "rake gems:install"
+
+puts "    Installing plugins"
+plugin(acts_as_tree)
+plugin(paperclip)
+plugin(permalink_fu)
+
 puts "    Applying contentator templates to project"
-
-gem "contentator"
 generate :contentator_files
-
-# test = ContentatorFilesGenerator.new
-
-
-# plugin(name, options = {})
-# acts_as_tree    paperclip       permalink_fu    tiny_mce
-
-
-#files
-#  config/routes.rb
-
-
-# Remove the file on both *unix and Windows
-# if Gem.win_platform?
-#   run "del public\\index.html"
-# else
-#   run "rm public/index.html"
-# end
-# 
-# gem "browsercms"
-# if Gem.win_platform?
-#   puts "        rake  db:create"
-#   `rake.cmd db:create`
-# else
-#   rake "db:create"
-# end
-# route "map.routes_for_browser_cms"
-# generate :browser_cms
-# environment 'SITE_DOMAIN="localhost:3000"', :env => "development"
-# environment 'SITE_DOMAIN="localhost:3000"', :env => "test"
-# environment 'SITE_DOMAIN="localhost:3000"', :env => "production"
-# environment 'config.action_view.cache_template_loading = false', :env => "production"
-# environment 'config.action_controller.page_cache_directory = RAILS_ROOT + "/public/cache/"', :env => "production"
-# initializer 'browsercms.rb', <<-CODE
-# Cms.attachment_file_permission = 0640
-# CODE
-# if Gem.win_platform?
-#   puts "        rake  db:migrate"
-#   `rake.cmd db:migrate`
-# else
-#   rake "db:migrate"
-# end
