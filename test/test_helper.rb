132d7a8b50fbb2225a5493e9e75a4266bd38dfda
Craig Partin <craigpartin@Craig-Partins-MacBook-Pro.local>
2009-10-19 16:36:15 -0600

    remove paperclip from test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 require 'shoulda'
 require 'mocha'
-require 'paperclip'
 
 class ActiveSupport::TestCase
   def deny(condition)
