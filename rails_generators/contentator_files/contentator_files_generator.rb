dc3578ebaf21abcd6caeb3132d5a26dc74363f48
Craig Partin <craigpartin@Craig-Partins-MacBook-Pro.local>
2009-10-19 15:35:53 -0600

    remove plugins tiny_mce and paperclip

--- rails_generators/contentator_files/contentator_files_generator.rb
+++ rails_generators/contentator_files/contentator_files_generator.rb
@@ -77,6 +77,7 @@ class ContentatorFilesGenerator < Rails::Generator::Base
       m.file '../../../public/stylesheets/application.css', 'public/stylesheets/application.css'
       
       m.file '../../../test/factories.rb', 'test/factories.rb'
+      m.file '../../../test/test_helper.rb', 'test/test_helper.rb', :collision => :force
 
       m.directory('test/functional/cms/admin')      
       m.file '../../../test/functional/cms/admin/page_content_block_controller_test.rb', 'test/functional/cms/admin/page_content_block_controller_test.rb'
@@ -86,8 +87,8 @@ class ContentatorFilesGenerator < Rails::Generator::Base
       m.file '../../../test/unit/page_content_block_test.rb', 'test/unit/page_content_block_test.rb'
       m.file '../../../test/unit/page_test.rb', 'test/unit/page_test.rb'
 
-      # refused to just cppy the file for some reason.  
-      m.template 'application.rb', 'public/javascripts/admin.js', :collision => :force
+      #refused to just copy the file for some reason.  
+      m.template 'application.rb', 'public/javascripts/application.js', :collision => :force
     end
   end  
 end
